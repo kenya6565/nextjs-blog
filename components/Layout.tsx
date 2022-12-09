620bb588866b1fb452ec46b3218645d5578c39fb
kenya6565 <ken.australia0605@gmail.com>
2022-12-10 02:41:27 +0900

    create util.module.css

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 const name: string = "Ken Code";
 export const siteTitle: string = " Next.js blog";
@@ -11,8 +12,12 @@ function Layout({ children }: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="images/profile.png" alt="Vercel Logo" />
-        <h1>{name}</h1>
+        <img
+          src="images/profile.png"
+          alt="Vercel Logo"
+          className={utilStyles.borderCircle}
+        />
+        <h1 className={utilStyles.hedding2xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
