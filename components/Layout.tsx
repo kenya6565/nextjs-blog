18bfc38c542f475c9469f3d3bbf375abbb71b578
kenya6565 <ken.australia0605@gmail.com>
2022-12-09 23:25:35 +0900

    create layout.module.css

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "./layout.module.css";
 
 const name: string = "Ken Code";
 export const siteTitle: string = " Next.js blog";
 function Layout({ children }: any) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
+      <header className={styles.header}>
         <img src="images/profile.png" alt="Vercel Logo" />
         <h1>{name}</h1>
       </header>
