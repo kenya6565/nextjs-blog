edc113f0bb220a9226be385a2c70bcb4658ac60c
kenya6565 <ken.australia0605@gmail.com>
2022-12-10 03:07:10 +0900

    create index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <Layout>hoge</Layout>
-  )
+    <Layout>
+      <section className={utilStyles.heddingMd}>
+        <p>私はエンジニアです。</p>
+      </section>
+      <section>
+        <h2>🗒エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" />
+            </Link>
+            <Link href="/">
+              <p>SSGとSSRの使い分けの場面はいつなのか？</p>
+            </Link>
+            <br />
+            <small>10th of December, 2022</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
