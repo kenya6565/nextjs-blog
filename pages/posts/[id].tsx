3cfadca54b6276321154dd87c65f7cf80ad44211
kenya6565 <ken.australia0605@gmail.com>
2022-12-13 03:33:38 +0900

    add getStaticPaths()

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,18 @@
 import Layout from '../../components/Layout';
+import { getAllPostIds } from '../../lib/post';
+
+// SSGで動的ルーティングを設定する
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+
+    // falseにすると上のパス以外にアクセすると404になるようになる
+    fallback: false,
+  }
+}
+
 export default function Post() {
   return (
     <Layout>動的ルーティング設定</Layout>
